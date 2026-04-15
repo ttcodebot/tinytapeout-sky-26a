51e9234de5fbd3d7b3fb451d63566f6bd0efa817
TinyTapeoutBot <139130078+TinyTapeoutBot@users.noreply.github.com>
2026-04-15 22:39:52 +0300

    feat: update project tt_um_ece298A_analog from jayrel053/jayrel053_ttsky26a_analog
    Commit: 81d6b24758bf0308d17046c716f975f69c3e9136
    Workflow: https://github.com/jayrel053/jayrel053_ttsky26a_analog/actions/runs/24474069217

---
+++ projects/tt_um_ece298A_analog/tt_um_ece298A_analog.v
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_ece298A_analog (
+    input  wire       VGND,
+    input  wire       VDPWR,    // 1.8v power supply
+//    input  wire       VAPWR,    // 3.3v power supply
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    inout  wire [7:0] ua,       // Analog pins, only ua[5:0] can be used
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+// All output pins must be assigned. If not used, assign to 0.
+  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 1;
+  assign uio_oe  = 0;
+
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, clk, rst_n, 1'b0};
+endmodule
